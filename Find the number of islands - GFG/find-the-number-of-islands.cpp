473d075c1a65b778fc62ac11731406395a591c24
Kshitij Kumar <54733760+Kshitij27Kumar@users.noreply.github.com>
2023-02-09 13:29:39 +0530

    Added solution - LeetHub

--- Find the number of islands - GFG/find-the-number-of-islands.cpp
+++ Find the number of islands - GFG/find-the-number-of-islands.cpp
@@ -6,39 +6,6 @@ using namespace std;
 class Solution {
   public:
     // Function to find the number of islands.
-    /*void solve(int i, int j, vector<vector<char>>& grid, int r, int c)
-    {
-        if(i<0 || j<0 || i>=r || j>=c || grid[i][j]!='1')
-            return;
-        grid[i][j]='2';
-       solve(i+1,j,grid,r,c);
-       solve(i-1,j,grid,r,c);
-       solve(i,j-1,grid,r,c);
-       solve(i,j+1,grid,r,c);
-       solve(i+1,j+1,grid,r,c);
-       solve(i-1,j-1,grid,r,c);
-       solve(i+1,j-1,grid,r,c);
-       solve(i-1,j+1,grid,r,c);
-    }
-    int numIslands(vector<vector<char>>& grid) {
-        // Code here
-        int row=grid.size();
-        int col=grid[0].size();
-        int island=0;
-        if(row==0)  return 0;
-        for(int i=0;i<row;i++)
-        {
-            for(int j=0;j<col;j++)
-            {
-                if(grid[i][j]=='1')
-                {
-                    island++;
-                    solve(i,j,grid,row,col);
-                }
-            }
-        }
-        return island;
-    }*/
     void dfs(int i, int j, vector<vector<char>>&grid, int n, int m){
         if(i<0||i>=n||j<0||j>=m||grid[i][j]!='1')
             return;
