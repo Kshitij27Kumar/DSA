66b56dae3a0efb4f1b6ad1aa4a7b6aa71e930e8e
Kshitij Kumar <54733760+Kshitij27Kumar@users.noreply.github.com>
2023-02-22 12:12:28 +0530

    Added solution - LeetHub

---
+++ Number of Distinct Islands - GFG/number-of-distinct-islands.cpp
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    void dfs(vector<vector<int>>& grid, vector<vector<int>>& vis, int row, int col, vector<pair<int,int>>& v, int row0, int col0){
+        vis[row][col]=1;
+        int n=grid.size(), m=grid[0].size();
+        v.push_back({row-row0,col-col0});
+        int delrow[]={-1,0,1,0};
+        int delcol[]={0,1,0,-1};
+        for(int i=0;i<4;i++){
+            int nrow=row+delrow[i];
+            int ncol=col+delcol[i];
+            if(nrow>=0 && ncol>=0 && nrow<n && ncol<m && !vis[nrow][ncol] && grid[nrow][ncol])
+                dfs(grid,vis,nrow,ncol,v,row0,col0);
+        }
+    }
+    int countDistinctIslands(vector<vector<int>>& grid) {
+        // code here
+        int n=grid.size(), m=grid[0].size();
+        vector<vector<int>>vis(n,vector<int>(m,0));
+        set<vector<pair<int,int>>>s;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j] && !vis[i][j]){
+                    vector<pair<int,int>>v;
+                    dfs(grid,vis,i,j,v,i,j);
+                    s.insert(v);
+                }
+            }
+        }
+        return s.size();
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        vector<vector<int>> grid(n, vector<int>(m));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                cin >> grid[i][j];
+            }
+        }
+        Solution obj;
+        cout << obj.countDistinctIslands(grid) << endl;
+    }
+}
+// } Driver Code Ends
