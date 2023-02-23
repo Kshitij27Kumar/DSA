8518933f8ad6d8fb1827ccb13379454201faeb7a
Kshitij Kumar <54733760+Kshitij27Kumar@users.noreply.github.com>
2023-02-23 11:23:47 +0530

    Update topological-sort.cpp

--- Topological sort - GFG/topological-sort.cpp
+++ Topological sort - GFG/topological-sort.cpp
@@ -5,29 +5,57 @@ using namespace std;
 // } Driver Code Ends
 class Solution
 {
+// 	dfs algo
+// 	public:
+// 	void dfs(int vis[], vector<int>adj[], stack<int>&s, int node){
+// 	    vis[node]=1;
+// 	    for(auto it: adj[node]){
+// 	        if(!vis[it])
+// 	            dfs(vis,adj,s,it);
+// 	    }
+// 	    s.push(node);
+// 	}
+// 	//Function to return list containing vertices in Topological order. 
+// 	vector<int> topoSort(int V, vector<int> adj[]) 
+// 	{
+// 	    // code here
+// 	    vector<int>ans;
+// 	    stack<int>s;
+// 	    int vis[V]={0};
+// 	    for(int i=0;i<V;i++){
+// 	        if(!vis[i])
+// 	            dfs(vis,adj,s,i);
+// 	    }
+// 	    while(!s.empty()){
+// 	        ans.push_back(s.top());
+// 	        s.pop();
+// 	    }
+// 	    return ans;
+// 	}
 	public:
-	void dfs(int vis[], vector<int>adj[], stack<int>&s, int node){
-	    vis[node]=1;
-	    for(auto it: adj[node]){
-	        if(!vis[it])
-	            dfs(vis,adj,s,it);
-	    }
-	    s.push(node);
-	}
-	//Function to return list containing vertices in Topological order. 
+	//bfs algo (Kahn's Algorithm)
 	vector<int> topoSort(int V, vector<int> adj[]) 
 	{
-	    // code here
-	    vector<int>ans;
-	    stack<int>s;
-	    int vis[V]={0};
+	    queue<int>q;
+	    int indegree[V]={0};
 	    for(int i=0;i<V;i++){
-	        if(!vis[i])
-	            dfs(vis,adj,s,i);
+	        for(auto it:adj[i])
+	            indegree[it]++;
 	    }
-	    while(!s.empty()){
-	        ans.push_back(s.top());
-	        s.pop();
+	    for(int i=0;i<V;i++){
+	        if(indegree[i]==0)
+	            q.push(i);
+	    }
+	    vector<int>ans;
+	    while(!q.empty()){
+	        int node=q.front();
+	        q.pop();
+	        ans.push_back(node);
+	        for(auto it: adj[node]){
+	            indegree[it]--;
+	            if(indegree[it]==0)
+	                q.push(it);
+	        }
 	    }
 	    return ans;
 	}
